56f32fd752cc55d777b736f59342e917833bf841
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-12 14:29:26 +0100

    built #assign_trainer

--- app/models/gym/client.rb
+++ app/models/gym/client.rb
@@ -1,5 +1,5 @@
 class Client
-    attr_reader :name
+    attr_reader :name, :trainer
     @@all = []
 
     def initialize(name)
@@ -11,10 +11,11 @@ class Client
         @@all
     end
 
-    def assign_trainer
+    def assign_trainer(trainer)
         # should take a trainer as argument and assign it to the client
+        trainer.clients << self
     end
-    
+
 
 
 end
--- app/models/gym/trainer.rb
+++ app/models/gym/trainer.rb
@@ -1,9 +1,11 @@
 class Trainer
 attr_reader :name
+attr_accessor :clients
 @@all = []
 
 def initialize(name)
     @name = name
+    @clients = []
     @@all << self
 end
 
@@ -13,7 +15,7 @@ end
 
 def self.most_clients
     #should find which trainer has the most clients.
+    self.all.max_by{|trainer| trainer.clients }
 end
-
 
 end
